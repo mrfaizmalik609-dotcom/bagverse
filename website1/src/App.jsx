0dc1a6f2c39a36739cfdc55cb2db242ce7b2cab9
Faiz Malik <mrfaizmalik609@gmail.com>
2025-08-21 20:10:24 +0500

    ok

--- website1/src/App.jsx
+++ website1/src/App.jsx
@@ -2,39 +2,37 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./Pages/Home";
-import Product from "./Pages/Product";
+import Product from "./Pages/Product";           // ✅ Capital P
 import About from "./Pages/About";
-import Login from "./Pages/login";
+import Login from "./Pages/Login";               // ✅ Capital L
 import Cart from "./Pages/Cart";
-import { CartProvider } from "./context/CartContext"; // ✅ correct import
+import { CartProvider } from "./context/CartContext";
 import Contact from "./Pages/Contact";
 import Signup from "./Pages/Signup";
-import Singleproduct from "./Pages/Singleproduct";
+import SingleProduct from "./Pages/SingleProduct"; // ✅ Capital P
 import Checkout from "./Pages/Checkout";
-import Thankyou from "./Pages/Thankyou";
-import Forgetpassword from "./Pages/Forgetpassword";
+import ThankYou from "./Pages/ThankYou";           // ✅ Capital Y
+import ForgetPassword from "./Pages/ForgetPassword"; // ✅ Capital P
 import Wishlist from "./Pages/Wishlist";
-
 
 function App() {
   return (
-    <CartProvider> {/* ✅ Wrap entire app in provider */}
+    <CartProvider>
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product" element={<Product />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/Contact" element={<Contact/>} />
-          <Route path="/Signup" element={<Signup/>}/>
-        <Route path="/product/:id" element={<Singleproduct />} />
-        <Route path="/Checkout" element={<Checkout />} />
-        <Route path="/Thankyou" element={<Thankyou />}/>
-       <Route path="/forgot-password" element={<Forgetpassword />} />
-       <Route path="/Wishlist" element={<Wishlist/>}/>
-
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/thankyou" element={<ThankYou />} />
+          <Route path="/forgot-password" element={<ForgetPassword />} />
+          <Route path="/wishlist" element={<Wishlist />} />
         </Routes>
         <Footer />
       </Router>
